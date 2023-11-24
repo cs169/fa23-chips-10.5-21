ee66b4d36e396a4b15d70e2c8f5213e5a38cb3b2
Kenneth Hsu <bryanhsu2020@berkeley.edu>
2023-11-24 23:54:44 +0000

    fixed JS syntax

--- app/javascript/packs/state_map.js
+++ app/javascript/packs/state_map.js
@@ -1,34 +1,34 @@
 const d3 = require('d3');
 const stateMapUtils = require('./state_map_utils');
-
 require('../stylesheets/map.scss');
 
-$(document).ready(() => {
-    const stateMap = new stateMapUtils.Map();
-    d3.json(stateMap.topojsonUrl).then((topology) => {
-        const mapAssets = stateMapUtils.parseTopojson(stateMap, topology);
-        stateMap.svgElement.selectAll('path')
-            .data(mapAssets.geojson.features)
-            .enter()
-            .append('path')
-            .attr('class', 'actionmap-view-region')
-            .attr('d', mapAssets.path)
-            .attr('data-county-name', (d) => stateMap.counties[d.properties.COUNTYFP].name)
-            .attr('data-county-fips-code', (d) => d.properties.COUNTYFP);
+document.addEventListener("DOMContentLoaded", function () {
+  const stateMap = new stateMapUtils.Map();
 
-        stateMapUtils.setupEventHandlers(stateMap);
-    });
-});
+  d3.json(stateMap.topojsonUrl).then((topology) => {
+    const mapAssets = stateMapUtils.parseTopojson(stateMap, topology);
 
-document.addEventListener("DOMContentLoaded", function() {
-  var countyLinks = document.querySelectorAll(".county-link");
+    stateMap.svgElement
+      .selectAll('path')
+      .data(mapAssets.geojson.features)
+      .enter()
+      .append('path')
+      .attr('class', 'actionmap-view-region')
+      .attr('d', mapAssets.path)
+      .attr('data-county-name', (d) => stateMap.counties[d.properties.COUNTYFP].name)
+      .attr('data-county-fips-code', (d) => d.properties.COUNTYFP);
 
-  countyLinks.forEach(function(link) {
-    link.addEventListener("click", function(event) {
+    stateMapUtils.setupEventHandlers(stateMap);
+  });
+
+  const countyLinks = document.querySelectorAll(".county-link");
+
+  countyLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
-      var stateSymbol = link.getAttribute("data-state-symbol");
-      var countyFIPSCode = link.getAttribute("data-county-fips-code");
-      var searchURL = "/search?state_symbol=" + stateSymbol + "&county_fips_code=" + countyFIPSCode;
+      const stateSymbol = link.getAttribute("data-state-symbol");
+      const countyFIPSCode = link.getAttribute("data-county-fips-code");
+      const searchURL = "/search?state_symbol=" + stateSymbol + "&county_fips_code=" + countyFIPSCode;
       window.location.href = searchURL;
     });
   });
