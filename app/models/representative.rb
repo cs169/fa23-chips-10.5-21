5369a7fdab07f1a8727891d3e978633856b83ea3
Santiago Barajas <santiago.barajas@berkeley.edu>
2023-11-25 00:26:36 +0000

    Modified code passes first test

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
@@ -6,22 +5,26 @@ class Representative < ApplicationRecord
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
-    rep_info.officials.each_with_index do |official, index|
+    rep_info[:officials].each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
 
-      rep_info.offices.each do |office|
-        if office.official_indices.include? index
-          title_temp = office.name
-          ocdid_temp = office.division_id
+      rep_info[:offices].each do |office|
+        if office[:official_indices].include?(index)
+          title_temp = office[:name]
+          ocdid_temp = office[:division_id]
         end
       end
+      
+      already_exists = Representative.find_by(name: official[:name], title: title_temp)
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
-      reps.push(rep)
+      if already_exists.nil?
+        rep = Representative.create!({ name: official[:name], ocdid: ocdid_temp, title: title_temp })
+        reps.push(rep)
+      else
+        reps.push(already_exists)
+      end
     end
-
     reps
   end
 end
---
+++ spec/models/representative_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe Representative do
+  it 'adds representative to db if not yet in db' do
+    rep_info = {
+      officials: [{ name: 'John Doe' }],
+      offices: [{ name: 'Representative', division_id: 'ocd-division/country:us/state:ca/district:33',
+      official_indices: [0] }]
+    }
+
+    # Mock the Representative.find_by call to return nil
+    allow(Representative).to receive(:find_by).and_return(nil)
+
+    # Mock the Representative.create call
+    allow(Representative).to receive(:create!).and_return(Representative.new(name: 'John Doe', ocdid: 'ocd-division/country:us/state:ca/district:33', title: 'Representative'))
+
+    representatives = Representative.civic_api_to_representative_params(rep_info)
+
+    expect(representatives.count).to eq(1)
+    expect(representatives.first.name).to eq('John Doe')
+    expect(representatives.first.ocdid).to eq('ocd-division/country:us/state:ca/district:33')
+    expect(representatives.first.title).to eq('Representative')
+  end
+end
