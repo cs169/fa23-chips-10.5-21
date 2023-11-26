a4e190b2cf9e4a62e67ec929bdc6cea67a88e692
Santiago Barajas <santiago.barajas@berkeley.edu>
2023-11-26 21:36:08 +0000

    got rid of linting errors

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,47 +1,40 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'spec_helper'
 
 describe Representative do
-  it 'adds representative to db if not yet in db' do
-    rep_info = {
+  let(:john_doe_rep_info) do
+    {
       officials: [{ name: 'John Doe' }],
       offices:   [{ name: 'Representative', division_id: 'ocd-division/country:us/state:ca/district:33',
-      official_indices: [0] }]
+                     official_indices: [0] }]
     }
-
-    # Mock the Representative.find_by call to return nil
-    allow(Representative).to receive(:find_by).and_return(nil)
-
-    # Mock the Representative.create call
-    allow(Representative).to receive(:create!).and_return(Representative.new(name: 'John Doe',
-                                                                             ocdid: 'ocd-division/country:us/state:ca/district:33', title: 'Representative'))
-
-    representatives = Representative.civic_api_to_representative_params(rep_info)
-
-    expect(representatives.count).to eq(1)
-    expect(representatives.first.name).to eq('John Doe')
-    expect(representatives.first.ocdid).to eq('ocd-division/country:us/state:ca/district:33')
-    expect(representatives.first.title).to eq('Representative')
   end
 
-  it 'returns existing representative if already in db' do
-    rep_info = {
-      officials: [{ name: 'John Doe' }],
-      offices:   [{ name: 'Representative', division_id: 'ocd-division/country:us/state:ca/district:33',
-      official_indices: [0] }]
-    }
+  let(:existing_representative) do
+    described_class.new(
+      name:  'John Doe',
+      ocdid: 'ocd-division/country:us/state:ca/district:33',
+      title: 'Representative'
+    )
+  end
 
-    # Mock the Representative.find_by call to return an existing representative
-    existing_representative = Representative.new(name: 'John Doe',
-                                                  ocdid: 'ocd-division/country:us/state:ca/district:33', title: 'Representative')
-    allow(Representative).to receive(:find_by).and_return(existing_representative)
+  it 'adds a representative to the database if not yet in the database' do
+    allow(described_class).to receive(:find_by).and_return(nil)
+    allow(described_class).to receive(:create!).and_return(existing_representative)
 
-    # Mock the Representative.create call
-    allow(Representative).to receive(:create!)
+    representatives = described_class.civic_api_to_representative_params(john_doe_rep_info)
 
-    representatives = Representative.civic_api_to_representative_params(rep_info)
+    expect(representatives).to contain_exactly(existing_representative)
+  end
 
-    expect(representatives.count).to eq(1)
-    expect(representatives.first).to eq(existing_representative)
+  it 'returns an existing representative if already in the database' do
+    allow(described_class).to receive(:find_by).and_return(existing_representative)
+    allow(described_class).to receive(:create!)
+
+    representatives = described_class.civic_api_to_representative_params(john_doe_rep_info)
+
+    expect(representatives).to contain_exactly(existing_representative)
   end
 end
