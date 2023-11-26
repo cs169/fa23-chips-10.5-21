31b9ce203391545e8c70e00156be11b6bf9dc797
Owen Murphy <ohmurphy@berkeley.edu>
2023-11-26 01:01:52 +0000

    made reps model add new columns from api request

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
@@ -17,10 +16,28 @@ class Representative < ApplicationRecord
         end
       end
 
+      # lin1, city, state, zip
+      addr_street = ''
+      addr_city = ''
+      addr_state = ''
+      addr_zip = ''
+      unless official.address.nil?
+        addr_street = official.address.first.line1
+        addr_city = official.address.first.city
+        addr_state = official.address.first.state
+        addr_zip = official.address.first.zip
+      end
+      
       already_exists = Representative.find_by(name: official[:name], title: title_temp)
 
       if already_exists.nil?
-        rep = Representative.create!({ name: official[:name], ocdid: ocdid_temp, title: title_temp })
+        rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
+          title: title_temp,
+          address_street: addr_street, # official.address[0],
+          address_city: addr_city, # official.address[1],
+          address_state: addr_state, # official.address[2],
+          address_zip: addr_zip, # official.address[3],
+          party: official.party, photo_url: official.photo_url })
         reps.push(rep)
       else
         reps.push(already_exists)
