0f4fe435b1d1bb705551b7e276c3393c9456955f
Santiago Barajas <santiago.barajas@berkeley.edu>
2023-11-28 20:26:59 +0000

    Changing news_items schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2023_11_22_043625) do
   create_table "news_items", force: :cascade do |t|
     t.string "title", null: false
     t.string "link", null: false
-    t.string "issue", null: false
     t.text "description"
     t.integer "representative_id", null: false
     t.datetime "created_at", null: false
