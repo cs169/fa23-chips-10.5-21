d8aebac31556e8ed3e8f3495992eec29ba61d6e8
Owen Murphy <ohmurphy@berkeley.edu>
2023-12-03 05:23:04 +0000

    removed propublicaAPI as its own class, incorporated it in model

--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -8,16 +8,13 @@ class CampaignFinance < ApplicationRecord
     stored_vals = CampaignFinance.where(cycle: cycle, category: category)
 
     unless stored_vals.exists?
-      api_key = Rails.application.credentials[:PROPUBLICA_API_KEY]
-      propublica_service = PropublicaService.new(api_key)
-      Rails.logger.info("API Key: #{api_key}")
-      Rails.logger.info("cycle: #{cycle}, category: #{category}}")  # Log URL before making the request
+      Rails.logger.info("cycle: #{cycle}, category: #{category}}")  
 
-      api_response = propublica_service.get_candidate_info(cycle, category)
+      api_response = CampaignFinance.get_candidate_info_from_api(cycle, category)
       if api_response.nil?
         return nil
       end
-      Rails.logger.info("api_response: #{api_response}")  # Log URL before making the request
+      Rails.logger.info("api_response: #{api_response}") 
       processed_candidates = CampaignFinance.add_candidates_to_db(api_response, cycle, category)
     end
 
@@ -26,14 +23,55 @@ class CampaignFinance < ApplicationRecord
 
   def self.add_candidates_to_db(api_response, cycle, category)
     candidates_list = []
-    #json_info = JSON.parse(api_response)
-    #json_info['results'].each do |candidate|
-    #  candidates_list.push(candidate['name'])
-    #end
+    json_info = JSON.parse(api_response)
+    json_info['results'].each do |candidate|
+      candidates_list.push(candidate['name'])
+    end
 
     finances = CampaignFinance.create!(candidates_list: candidates_list, cycle: cycle, category: category)
     finances
   end
+
+  def self.get_candidate_info_from_api(cycle, category)
+    Rails.logger.info("get_candidate_info method called") 
+    puts "get_candidate_info method called"
+    url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
+    
+    Rails.logger.info("API Request URL: #{url}")
+    puts "API Request URL: #{url}"
+    api_key = Rails.application.credentials[:PROPUBLICA_API_KEY]
+    headers = { 'X-API-Key' =>  api_key}
+  
+    response = CampaignFinance.faraday_connection.get(url, nil, headers)
+    Rails.logger.info("API Request Headers: #{headers}")
+    puts "API Request Headers: #{headers}"
+    #response = FakeResponse.new(600, "nothing")
+
+    CampaignFinance.handle_response(response)
+  end
+  
+  def self.handle_response(response)
+    Rails.logger.info("handle_response method called") 
+    puts "handle_response method called"
+    if response.status == 200
+      Rails.logger.debug("#{response.body}")
+      return response.body
+    else
+      #raise "API request failed with status code #{response.status}: #{response.body}"
+      return nil
+    end
+  end
+
+  def self.faraday_connection
+    Rails.logger.info("connection method called")
+    puts "connection method called"
+    @connection ||= Faraday.new do |conn|
+      conn.use Faraday::Request::UrlEncoded
+      conn.use Faraday::Response::Logger 
+      conn.adapter Faraday.default_adapter
+    end
+  end
+  
 end
 
 class FakeResponse
@@ -45,49 +83,3 @@ class FakeResponse
   end
 end
 
-class PropublicaService
-  def initialize(api_key)
-    @api_key = api_key
-  end
-
-  def get_candidate_info(cycle, category)
-    Rails.logger.info("get_candidate_info method called") 
-    puts "get_candidate_info method called"
-    url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
-    
-    Rails.logger.info("API Request URL: #{url}")
-    puts "API Request URL: #{url}"
-    headers = { 'X-API-Key' => @api_key }
-  
-    #response = connection.get(url, nil, headers)
-    Rails.logger.info("API Request Headers: #{headers}")
-    puts "API Request Headers: #{headers}"
-    response = FakeResponse.new(600, "nothing")
-
-    handle_response(response)
-  end
-
-  private
-
-  def connection
-    Rails.logger.info("connection method called")
-    puts "connection method called"
-    @connection ||= Faraday.new do |conn|
-      conn.use Faraday::Request::UrlEncoded
-      conn.use Faraday::Response::Logger 
-      conn.adapter Faraday.default_adapter
-    end
-  end
-
-  def handle_response(response)
-    Rails.logger.info("handle_response method called") 
-    puts "handle_response method called"
-    if response.status == 200
-      Rails.logger.debug("#{response.body}")
-      return response.body
-    else
-      #raise "API request failed with status code #{response.status}: #{response.body}"
-      return nil
-    end
-  end
-end
