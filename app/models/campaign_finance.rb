db3c20f50fdd234012e30e43cdad8e297f73b0c3
Owen Murphy <ohmurphy@berkeley.edu>
2023-12-03 02:29:43 +0000

    replaced w/ Faraday

--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -1,65 +1,66 @@
-require 'httparty'
-require 'net/http'
+require 'faraday'
 require 'json'
 
 class CampaignFinance < ApplicationRecord
-
   serialize :candidates_list, JSON
 
   def self.get_cycle_and_category(cycle, category)
-    # if already stored, return values
     stored_vals = CampaignFinance.where(cycle: cycle, category: category)
-    if stored_vals.count == 0
+
+    unless stored_vals.exists?
       api_key = Rails.application.credentials[:PROPUBLICA_API_KEY]
       propublica_service = PropublicaService.new(api_key)
       Rails.logger.info("API Key: #{api_key}")
       api_response = propublica_service.get_candidate_info(cycle, category)
       processed_candidates = CampaignFinance.add_candidates_to_db(api_response, cycle, category)
     end
+
     CampaignFinance.where(cycle: cycle, category: category)
   end
 
-  def self.add_candidates_to_db(api_reponse, cycle, category)
+  def self.add_candidates_to_db(api_response, cycle, category)
     candidates_list = []
-    json_info = JSON.parse(api_reponse)
-    # Rails.logger.debug(api_reponse.methods)
-    # Rails.logger.debug(json_info.to_s)
-    # Rails.logger.debug(json_info.methods)
+    json_info = JSON.parse(api_response)
     json_info['results'].each do |candidate|
       candidates_list.push(candidate['name'])
     end
+
     finances = CampaignFinance.create!(candidates_list: candidates_list, cycle: cycle, category: category)
     finances
   end
 end
 
-
 class PropublicaService
-
   def initialize(api_key)
     @api_key = api_key
   end
 
   def get_candidate_info(cycle, category)
-    url = URI.parse("https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = (url.scheme == 'https')
-    request = Net::HTTP::Get.new(url.path)
-    request['X-API-Key'] = "#{@api_key}"
-    response = http.request(request)
-    #Rails.logger.info("API Key: #{@api_key}")
-    #response = self.class.get("2015/candidates/leaders/pac-total.json", headers: {'X-API-Key' => "#{@api_key}"})
+    url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
+    
+    response = connection.get(url) do |req|
+      req.headers['X-API-Key'] = @api_key
+    end
+
     handle_response(response)
   end
 
   private
 
+  def connection
+    @connection ||= Faraday.new do |conn|
+      conn.use Faraday::Request::UrlEncoded
+      conn.use Faraday::Response::Logger if Rails.env.development?
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
   def handle_response(response)
-    if response.code.to_i == 200
+    if response.status == 200
       Rails.logger.debug("#{response.body}")
       response.body
     else
-      raise "API request failed with status code #{response.code}: #{response.body}"
+      raise "API request failed with status code #{response.status}: #{response.body}"
     end
   end
 end
