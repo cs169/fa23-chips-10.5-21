35f4b3b0477525edc8031354968e8f802622d234
Kenneth Hsu <bryanhsu2020@berkeley.edu>
2023-12-05 22:00:21 +0000

    added master value

--- features/step_definitions/search_controller_steps.rb
+++ features/step_definitions/search_controller_steps.rb
@@ -13,12 +13,11 @@ When('I click the search button') do
 end
 
 Then('I should be on the search page') do
-  expect(current_path).to eq(search_representatives_path)
+  expect(current_path).to eq('/representatives')
 end
 
 Then('I should see a list of representatives from California') do
   expect(page).to have_selector('.representative', count: 19)
-  # Assuming each representative is displayed with a CSS class '.representative'
 end
 
 Then('I should see name {string}') do |name|
