b3283055e9a3d9c1d6d38f9480e3f8e78c6223c2
Demetri Petuskey <petuskeyd24@berkeley.edu>
2023-12-06 22:44:39 +0000

    changed how api key is added

--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -66,6 +66,7 @@ class CampaignFinance < ApplicationRecord
     @faraday_connection ||= Faraday.new do |conn|
       conn.use Faraday::Request::UrlEncoded
       conn.use Faraday::Response::Logger
+      conn.headers['X-API-Key'] = Rails.application.credentials[:PROPUBLICA_API_KEY]
       conn.adapter Faraday.default_adapter
     end
   end
